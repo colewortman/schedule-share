6a0051b039b18d0c3b8a9a2c07e5ba1a21f9d9c8
colewortman <wortcole12@gmail.com>
2026-01-21 22:56:26 -0600

    Scrapping nordcraft idea

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,24 +1,9 @@
 import Image from "next/image";
-import ToddleEmbed from "./components/ToddleEmbed";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <div className="w-full mb-12">
-          <ToddleEmbed tag="toddle-main">
-            <p>Loading Nordcraft...</p>
-          </ToddleEmbed>
-          <ToddleEmbed
-            tag="toddle-link"
-            props={{
-              url: "Nordcraft.com",
-              text: "Hello World!",
-              prerender: "none",
-            }}
-          />
-        </div>
-
         <Image
           className="dark:invert"
           src="/next.svg"
