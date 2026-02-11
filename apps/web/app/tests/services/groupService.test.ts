ddbe808b3b3236825d1be102b7fb070f03622249
colewortman <wortcole12@gmail.com>
2026-02-10 19:24:13 -0600

    Finishing group tests, added user tests

--- apps/web/app/tests/services/groupService.test.ts
+++ apps/web/app/tests/services/groupService.test.ts
@@ -36,4 +36,34 @@ describe("groupService", () => {
         expect(group).toEqual(mockGroup);
         expect(mockFetchFromApi).toHaveBeenCalledWith("/groups/1");
     });
+
+    it('should create a group', async () => {
+        const mockGroup: Group = { group_id: "3", group_name: "Group 3" };
+        mockFetchFromApi.mockResolvedValue(mockGroup);
+
+        const newGroup = await groupService.create({ group_name: "Group 3" });
+
+        expect(newGroup).toEqual(mockGroup);
+        expect(mockFetchFromApi).toHaveBeenCalledWith("/groups", { method: "POST", body: JSON.stringify({ group_name: "Group 3" }) });
+    });
+
+    it('should update a group', async () => {
+    
+        const mockGroup: Group = { group_id: "3", group_name: "Group 3"};
+        mockFetchFromApi.mockResolvedValue(mockGroup);
+
+        const updatedGroup = await groupService.update("3", { group_name: "Updated Group"});
+
+        expect(updatedGroup.group_id).toEqual(mockGroup.group_id)
+        expect(mockFetchFromApi).toHaveBeenCalledWith("/groups/3", { method: "PUT", body: JSON.stringify({ group_name: "Updated Group"}) });
+    });
+
+    it('should delete a group', async () => {
+
+        mockFetchFromApi.mockResolvedValue(undefined);
+
+        await groupService.delete("1");
+
+        expect(mockFetchFromApi).toHaveBeenCalledWith("/groups/1", { method: "DELETE" });
+    });
 });
